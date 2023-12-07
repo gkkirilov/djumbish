4586d69f4b4cac3989bac25bf3fa39686dfd502e
Georgi Kirilov <gkkirilov@gmail.com>
2023-12-08 00:14:31 +0200

    Change x logo

--- components/Queue.vue
+++ components/Queue.vue
@@ -21,17 +21,18 @@ const playSong = (index) => {
 
       <div class="flex h-16 items-center px-3 mb-10 opacity-50 hover:opacity-100 justify-between
         transition-opacity ease-linear duration-300">
-        <a href="https://twitter.com/gkkirilov" target="_blank"><img class="h-6 w-auto " src="/icon.png"
+        <a href="https://x.com/gkkirilov" target="_blank"><img class="h-6 w-auto " src="/icon.png"
             alt="Your Company"></a>
-        <a href="https://twitter.com/gkkirilov" target="_blank"><span
+        <a href="https://x.com/gkkirilov" target="_blank"><span
             class="text-base font-semibold leading-12 tracking-tighter text-white">DJ <span
               class="pl-0.5">Umbish.club</span></span></a>
-        <a href="https://twitter.com/gkkirilov" target="_blank">
-          <svg viewBox="0 0 24 24" id="twitter" class="w-6">
+        <a href="https://x.com/gkkirilov" target="_blank">
+          <!-- <svg viewBox="0 0 24 24" id="twitter" class="w-6">
             <path fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round"
               d="M2 3.5a13.62 13.62 0 0 0 10 5c-.26-1.06-.28-3.94 1.74-5.14a5.24 5.24 0 0 1 2.76-.86A4.65 4.65 0 0 1 19.91 4a10.5 10.5 0 0 0 3-1.14 4.57 4.57 0 0 1-2 2.57 8.77 8.77 0 0 0 2.66-.66 9.3 9.3 0 0 1-2.38 2.43c.71 6.37-5 14.29-13.12 14.29a14.76 14.76 0 0 1-7.5-2 9.3 9.3 0 0 0 7-2 5.82 5.82 0 0 1-5-3.5c1 .32 1.95.48 2.5 0A5.41 5.41 0 0 1 1 9a3.51 3.51 0 0 0 2.5 1A5 5 0 0 1 2 3.5Z">
             </path>
-          </svg>
+          </svg> -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584l-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>
         </a>
       </div>
 
